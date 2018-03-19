3d4a3e5ebf9f020a882266837c01a94de7be0301
Sascha Huth <sascha.huth@transferwise.com>
2018-03-19 14:51:25 +0000

    rename app module to sample

--- sample/src/main/java/com/transferwise/sequencelayout/sample/MainActivity.kt
+++ sample/src/main/java/com/transferwise/sequencelayout/sample/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.transferwise.sequencelayout.sample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
