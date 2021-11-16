b65dfd9c5ee9de453b3778c0c8ab9042ef21791f
Antal János Monori <antal.monori@transferwise.com>
2021-11-16 19:49:32 +0000

    chore: GitHub Actions build setup (#19)
    * Inline versions, enable GHA/dependabot and refactor structure
    
    - Removed buildSrc and inlined Versions.kt
    - Switched to Groovy and enabled dependabot.yml
    - Renamed sequence layout to library
    - Added build.yml with basic verification step
    - Bump compile/target SDK to 31
    - Removed kotlin-stdlib dependency since that's now redundant from Kotlin 1.4.x (see: https://kotlinlang.org/docs/whatsnew14.html#dependency-on-the-standard-library-added-by-default)
    - Use gradle bin instead of all
    - Removed redundant settings from gradle.build like versionName/versionCode and release buildType settings
    - Add flag required for API 31
    
    Signed-off-by: Antal János Monori <antal.monori@transferwise.com>

--- sample/src/main/java/com/transferwise/sequencelayout/sample/MainActivity.kt
+++ sample/src/main/java/com/transferwise/sequencelayout/sample/MainActivity.kt
@@ -1,13 +1,5 @@
 package com.transferwise.sequencelayout.sample
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
